ab59a53ffe7d32f1881fe7173f403ca15e3d828f
Daniel Alvarez <a3dany@gmail.com>
2019-07-14 17:46:55 -0400

    Add SEO component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const AboutPage = () => (
   <Layout>
+    <SEO title="About"/>
     <h2>About</h2>
     <p>This page was created with Gatsby.</p>
   </Layout>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const ContactPage = () => (
   <Layout>
+    <SEO title="Contact"/>
     <h2>Contact</h2>
     <p>
       The best wat to reach me is via <a href="https://twitter.com/adanieldev" target="_blank">@adanieldev</a> on
